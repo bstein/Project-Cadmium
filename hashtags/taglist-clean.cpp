56bbf3b4027e86d203d87d48bc8dc44405fce8f1
Unknown <bastein@umich.edu>
2017-02-06 13:19:12 -0500

    Tweak taglist cleaner, add clean files

--- hashtags/taglist-clean.cpp
+++ hashtags/taglist-clean.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,42 +16,45 @@ int main(int argc, char *argv[])
   if (!ifs.is_open())
   {
     cout << "Unable to open file!" << endl;
-    return;
+    return 0;
   }
 
   // write in array style
-  ofstream ofsAS();
-  ofstream ofsLS();
-
-  ofsAS.open("taglist-arraystyle.txt");
-  ofsLS.open("taglist-linestyle.txt");
+  ofstream ofsAS("taglist-arraystyle.txt");
+  ofstream ofsLS("taglist-linestyle.txt");
 
   cout << "Reading and writing..." << endl;
 
   // read in line by line and detect hashtags
   string line;
-  bool firstAddition = true;
+  int i = 1;
   while (getline(ifs, line))
   {
-    if (line.contains('#'))
+    if (line.find('#') != string::npos)
     {
       // since this line includes a hashtag, write it to the clean files
-      line.erase(remove(line.begin(), line.end(), '$'), line.end());
-      if (firstRun))
+      line.erase(remove(line.begin(), line.end(), '#'), line.end());
+
+      if (i == 1)
       {
         ofsAS << line;
         ofsLS << line;
-        firstAddition = false;
       }
       else
       {
         ofsAS << ", " << line;
         ofsLS << "\n" << line;
       }
+
+      // uncomment this line to break up array style file
+      //if (i % 10 == 0) { ofsAS << "\n"; }
+
+      ++i;
     }
   }
 
   // finish
+  ifs.close();
   ofsAS.close();
   ofsLS.close();
   cout << "Finished!" << endl;
